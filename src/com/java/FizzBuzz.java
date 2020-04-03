3bac0132d5650491a5ded6886d1ae7ff993f9dd1
blaise-m <mubangiziblaise@gmail.com>
2020-04-03 10:58:37 +0300

    Implement the fizzBuzz method

--- src/com/java/FizzBuzz.java
+++ src/com/java/FizzBuzz.java
@@ -1,7 +1,23 @@
 package com.java;
 
+import java.util.Scanner;
+
 public class FizzBuzz {
     public static void fizzBuzz() {
+        System.out.println("The Fizz Buzz Game");
+        System.out.println("********************");
+        System.out.print("\nEnter a number of your choice: ");
+        Scanner scanner = new Scanner(System.in);
 
+        int num = scanner.nextInt();
+        if ((num%5 == 0) && (num%3 == 0)) {
+            System.out.println("FizzBuzz");
+        } else if (num%5 == 0) {
+            System.out.println("Fizz");
+        } else if (num%3 == 0) {
+            System.out.println("Buzz");
+        } else {
+            System.out.println(num);
+        }
     }
 }
